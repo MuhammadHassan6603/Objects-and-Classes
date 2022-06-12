e27dbccb8607c67b3b8f65d42013267c6aa1433f
Muhammad Hassan <105521572+MuhammadHassan6603@users.noreply.github.com>
2022-06-12 16:26:10 +0500

    Add files via upload

---
+++ hehe.java
@@ -0,0 +1,22 @@
+class Bakri
+{
+	int tadaad=10;
+	String Colour="BLACK";
+	void happy()
+	{
+		System.out.println("Red bakris are always happy :)");
+	}
+	void Colour()
+	{
+		System.out.println(Colour);
+	}
+}
+public class Hehe
+{
+	public static void main (String args[])
+	{
+		Bakri obj=new Bakri();
+		obj.happy();
+		obj.Colour();
+	}
+}
